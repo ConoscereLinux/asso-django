4b78e13e8ac88a82c76b3d7705f0f5e3edbb1d6d
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2022-11-25 23:28:10 +0100

    Add Member model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    cf = models.CharField(unique=True, max_length=16)
+    email = models.EmailField()
+
+    # birth_date: types.Date
+    # gender: types.Gender | None  # (meta) genere_member
+    # address: str  # (meta) indirizzo_member
+    # birth_place: str  # (meta) luogo_nascita_member
+
+    @property
+    def full_name(self):
+        return f"{str(self.name)} {str(self.surname).title()}"
+
+    def __str__(self):
+        return self.full_name
