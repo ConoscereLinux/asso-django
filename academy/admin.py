3aa157c39daa94ca4228652a9031019205d2ad07
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2023-03-15 02:28:58 +0100

    Add Trainer Admin Model

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+import common.admin as ca
+
+from . import models
+
+
+@admin.register(models.Trainer)
+class TrainerAdmin(ca.EditInfoAdmin, ca.TrashBinAdmin):
+    list_display = ("user",)
