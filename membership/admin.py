cc5cceabd87231095eb47c6de1af41a946335bc0
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-21 01:49:16 +0200

    Move Member to Membership Module

--- membership/admin.py
+++ membership/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+import common.admin
+
+from . import models
+
 # Register your models here.
+
+
+# @admin.register(models.Membership)
+# class MembershipAdmin(admin.ModelAdmin):
+#     list_display = ["year", "card", "member"]
+#     list_filter = ["year", "card"]
+
+
+# class MembershipInline(admin.StackedInline):
+#     model = models.Membership
+#     extra = 0
+
+
+@admin.site.register(models.Member)
+class MemberAdmin(common.admin.EditInfoAdmin, common.admin.TrashBinAdmin):
+    list_display = ["full_name", "id", "cf", "first_name", "last_name", "email"]
+    list_filter = ["first_name", "last_name", "cf", "email"]
+
+    # inlines = [MembershipInline]
