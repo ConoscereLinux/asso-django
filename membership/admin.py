77bb12ad54a74653e38b6f33d458007e15279cea
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-21 02:32:40 +0200

    Enable AdminModels in membership app

--- membership/admin.py
+++ membership/admin.py
@@ -7,20 +7,24 @@ from . import models
 # Register your models here.
 
 
-# @admin.register(models.Membership)
-# class MembershipAdmin(admin.ModelAdmin):
-#     list_display = ["year", "card", "member"]
-#     list_filter = ["year", "card"]
+@admin.register(models.Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ["member", "card_number", "period"]
 
 
-# class MembershipInline(admin.StackedInline):
-#     model = models.Membership
-#     extra = 0
+class MembershipInline(admin.StackedInline):
+    model = models.Membership
+    extra = 0
 
 
-# @admin.site.register(models.Member)
-# class MemberAdmin(common.admin.EditInfoAdmin, common.admin.TrashBinAdmin):
-#     list_display = ["full_name", "id", "cf", "first_name", "last_name", "email"]
-#     list_filter = ["first_name", "last_name", "cf", "email"]
-#
-#     # inlines = [MembershipInline]
+@admin.register(models.Member)
+class MemberAdmin(common.admin.EditInfoAdmin, common.admin.TrashBinAdmin):
+    list_display = ["full_name", "cf", "first_name", "last_name", "email"]
+    list_filter = ["first_name", "last_name", "cf", "email"]
+
+    inlines = [MembershipInline]
+
+
+@admin.register(models.MembershipPeriod)
+class MembershipPeriodAdmin(admin.ModelAdmin):
+    list_display = ["start_date", "end_date"]
