a0ae87985a93ca3e6e49a325fac733fff27d2b25
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-22 16:33:32 +0200

    Add NavbarItem model

--- website/admin.py
+++ website/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.NavbarItem)
+class NavbarItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("order", "title", "url")
--- website/models.py
+++ website/models.py
@@ -1,1 +1,22 @@
+from django.conf import settings
 from django.db import models
+
+
+class NavbarItem(models.Model):
+    class Meta:
+        ordering = ["order"]
+
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=50, blank=True)
+    url = models.CharField(max_length=200, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    order = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"({self.order:02}) {self.title}"
+
+    def get_absolute_url(self):
+        # https://docs.djangoproject.com/en/4.1/ref/models/instances/#get-absolute-url
+        if self.url and settings.BASE_URL:
+            return str(settings.BASE_URL / self.url.lstrip("/"))
+        return self.url
