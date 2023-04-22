2ae84f5e3d158da8c300b3df60d6002b2e517694
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-22 16:53:58 +0200

    Add context manager to retrieve site brand and menu items

--- website/views.py
+++ website/views.py
@@ -2,6 +2,19 @@ from django.views import generic
 
 from academy.models import Event
 
+from .models import NavbarItem
+
+
+def add_header_info_and_menu_items_to_context(request):
+    """Context Processor for adding menu items and brand info
+
+    See https://docs.djangoproject.com/en/4.2/ref/templates/api/#writing-your-own-context-processors
+    """
+    return {
+        "site_brand": "ConoscereLinux",  # TODO: should be taken from .env
+        "menu_items": NavbarItem.objects.filter(active=True),
+    }
+
 
 class IndexView(generic.ListView):
     template_name = "website/index.html"
