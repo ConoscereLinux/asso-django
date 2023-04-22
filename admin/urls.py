0c09363b193b2798302c7f4fc096e2aa435d476f
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-04-22 03:45:09 +0200

    Add member view

--- admin/urls.py
+++ admin/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("website.urls")),
+    path("member/", include("membership.urls")),
     path("admin/", admin.site.urls),
 ]
