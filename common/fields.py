805328feb42215757b9f00bc3b0c1e90658d2c0d
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-05-08 18:16:58 +0200

    Add reference to source for UniqueBooleanField

--- common/fields.py
+++ common/fields.py
@@ -2,6 +2,11 @@ from django.db.models import BooleanField
 
 
 class UniqueBooleanField(BooleanField):
+    """A boolean field which can be True on only one item
+
+    Based upon: https://stackoverflow.com/a/38340608
+    """
+
     def pre_save(self, model_instance, add):
         objects = model_instance.__class__.objects
 
