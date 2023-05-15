87ac23cbb401e08cb55550dac8e3fb96c7cb8156
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-05-15 19:24:04 +0200

    Make NavbarItem url field required

--- website/models.py
+++ website/models.py
@@ -18,7 +18,7 @@ class ThemeConfig(models.Model):
 class NavbarItem(cm.OrderedModel):
     slug = models.SlugField(unique=True)
     title = models.CharField(max_length=50, blank=True)
-    url = models.CharField(max_length=200, null=True, blank=True)
+    url = models.CharField(max_length=200, default="")
     active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -26,7 +26,7 @@ class NavbarItem(cm.OrderedModel):
 
     def get_absolute_url(self):
         # https://docs.djangoproject.com/en/4.1/ref/models/instances/#get-absolute-url
-        if self.url and settings.BASE_URL:
+        if self.url and not self.url.startswith("http") and settings.BASE_URL:
             return str(settings.BASE_URL / self.url.lstrip("/"))
         return self.url
 
