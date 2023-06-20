a1d6db002441d53b082d5ed21c6ae04296d3c034
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2023-06-21 01:24:20 +0200

    Refactor and improve load_item

--- asso/scripts/load_backup.py
+++ asso/scripts/load_backup.py
@@ -27,7 +27,6 @@ def fill_none(item: dict, key, default, skip_log: bool = False):
 
 def fill_member(member: dict) -> dict:
     fill_none(member, "document_expires", dt.date(1970, 1, 1), skip_log=True)
-
     member.pop("title", None)
     return member
 
@@ -42,7 +41,9 @@ def run(*args):
         data = json.load(fp)
 
     print("Loading members...")
-    members, users = {}, {}
+    members: dict[str, dict | Member] = {}
+    users: dict[str, dict | User] = {}
+
     for member in data.get("members", []):
         member_id = member.pop("id")
         users[member_id] = {
@@ -59,24 +60,26 @@ def run(*args):
             continue
 
         id_ = membership["member_id"]
-        member_data, user_data = members[id_], users[id_]
-        fill_member(member_data)
 
-        user, _ = load_item(user_data, User, ("email",))
-        member_data.setdefault("user", user)
-        try:
-            load_item(member_data, Member, ("user",))
-        except (ValidationError, IntegrityError) as err:
-            name = getattr(user, "full_name")
-            logger.error(f"Skip Member {name}: {getattr(err, 'error_list', err)}")
-            continue
+        if isinstance(user := users[id_], dict):
+            users[id_] = user = load_item(user, User, ("email",))
+
+        if isinstance(member := members[id_], dict):
+            try:
+                fill_member(member)
+                member.setdefault("user", user)
+                members[id_] = member = load_item(member, Member, ("user",))
+            except (ValidationError, IntegrityError) as err:
+                name = getattr(user, "full_name")
+                logger.error(f"Skip Member {name}: {getattr(err, 'error_list', err)}")
+                continue
 
         # load_item(membership, Membership, ...)
 
     print("Loading Courses")
     for event in tqdm(data.get("courses", [])):
         approval_state = {"title": event.pop("approval_state")}
-        event["approval_state"], _ = load_item(approval_state, ApprovalState)
+        event["approval_state"] = load_item(approval_state, ApprovalState)
 
         event.setdefault("slug", slugify(event.get("title", "")))
         event["creation_date"] = event.pop("creation_date")
