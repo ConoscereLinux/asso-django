c759a8c3808c30e2ef2e001603a31aa0dfc8737c
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-10-05 19:05:20 +0200

    Move homepage to landing

--- asso/landing/views.py
+++ asso/landing/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import datetime as dt
 
-# Create your views here.
+from django.utils.translation import gettext_lazy as _
+from django.views import generic
+
+from asso.academy.models import Event
+
+
+class HomePage(generic.TemplateView):
+    template_name = "home.html"
+
+    MODES = {
+        "next": _("Prossimi Eventi"),
+        "current": _("In corso"),
+        "archive": _("Archivio"),
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["modes"] = self.MODES
+
+        today = dt.date.today()
+        match mode := self.request.GET.get("mode"):
+            case "archive":
+                query = Event.objects.filter(end_date__lt=today)
+            case "current":
+                query = Event.objects.filter(start_date__lt=today, end_date__gt=today)
+            case "next" | _:
+                query = Event.objects.filter(start_date__gt=today)
+                mode = "next"
+
+        context["mode"] = mode
+        context["courses"] = query.order_by("-start_date")[:10]
+
+        return context
--- asso/website/views.py
+++ asso/website/views.py
@@ -1,10 +1,3 @@
-import datetime as dt
-
-from django.utils.translation import gettext_lazy as _
-from django.views import generic
-
-from asso.academy.models import Event
-
 from .models import NavbarItem, SocialLink, ThemeConfig
 
 
@@ -18,32 +11,3 @@ def add_header_info_and_menu_items_to_context(request):
         "socials": SocialLink.objects.all(),
         "theme": ThemeConfig.objects.filter(active=True).first(),
     }
-
-
-class HomePage(generic.TemplateView):
-    template_name = "home.html"
-
-    MODES = {
-        "next": _("Prossimi Eventi"),
-        "current": _("In corso"),
-        "archive": _("Archivio"),
-    }
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["modes"] = self.MODES
-
-        today = dt.date.today()
-        match mode := self.request.GET.get("mode"):
-            case "archive":
-                query = Event.objects.filter(end_date__lt=today)
-            case "current":
-                query = Event.objects.filter(start_date__lt=today, end_date__gt=today)
-            case "next" | _:
-                query = Event.objects.filter(start_date__gt=today)
-                mode = "next"
-
-        context["mode"] = mode
-        context["courses"] = query.order_by("-start_date")[:10]
-
-        return context
