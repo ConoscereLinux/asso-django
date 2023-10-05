0868a37b1415e765e7e4dbff3fa6ac6cd40dd350
Elia Giacobazzi <elia.giacobazzi@euler.it>
2023-10-05 19:51:58 +0200

    Rename website as theme

--- asso/theme/apps.py
+++ asso/theme/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WebsiteConfig(AppConfig):
+class ThemeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "asso.website"
+    name = "asso.theme"
