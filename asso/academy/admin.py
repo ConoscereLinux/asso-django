f45fe0b628c546e7dc772ba7a132676b2ecb3911
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-10-05 23:53:26 +0200

    Disable academy models, refactor Event, add EventCategory

--- asso/academy/admin.py
+++ asso/academy/admin.py
@@ -1,35 +1,47 @@
 from django.contrib import admin
 
-from ..core.admin import (
-    CreatedAdmin,
-    CreatedTabularInline,
-    EditableAdmin,
-    EditableTabularInline,
-    TrashableAdmin,
-)
+# from ..core.admin import (
+#     CreatedAdmin,
+#     CreatedTabularInline,
+#     EditableAdmin,
+#     EditableTabularInline,
+#     TrashableAdmin,
+# )
 from . import models
 
 
-@admin.register(models.ApprovalState)
-class ApprovalStateAdmin(CreatedAdmin, EditableAdmin, TrashableAdmin):
-    list_display = ("title", "show", "description")
-
-
-class SessionAdmin(CreatedTabularInline, EditableTabularInline):
-    model = models.Session
-
-
-class EnrollmentAdmin(CreatedTabularInline, EditableTabularInline):
-    model = models.Enrollment
+@admin.register(models.EventCategory)
+class EventCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
 
 
 @admin.register(models.Event)
-class EventAdmin(CreatedAdmin, EditableAdmin, TrashableAdmin):
-    list_display = ("title", "slug")
-    inlines = [SessionAdmin, EnrollmentAdmin]
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "slug",
+        "category",
+        "is_removed",
+        "need_membership",
+        "price",
+    )
     prepopulated_fields = {"slug": ("title",)}
+    # inlines = [SessionAdmin, EnrollmentAdmin]
 
 
-@admin.register(models.Trainer)
-class TrainerAdmin(CreatedAdmin, EditableAdmin, TrashableAdmin):
-    list_display = ("template_name", "user", "display_name")
+# @admin.register(models.ApprovalState)
+# class ApprovalStateAdmin(CreatedAdmin, EditableAdmin, TrashableAdmin):
+#     list_display = ("title", "show", "description")
+
+
+# class SessionAdmin(CreatedTabularInline, EditableTabularInline):
+#     model = models.Session
+
+
+# class EnrollmentAdmin(CreatedTabularInline, EditableTabularInline):
+#     model = models.Enrollment
+
+
+# @admin.register(models.Trainer)
+# class TrainerAdmin(CreatedAdmin, EditableAdmin, TrashableAdmin):
+#     list_display = ("template_name", "user", "display_name")
--- asso/academy/models.py
+++ asso/academy/models.py
@@ -1,56 +1,48 @@
 """The Academy section, with teacher event and participation."""
 
-from django.conf import settings
 from django.db import models
-from django.db.models import Q
-from django.db.models.functions import Length
 from django.utils.translation import gettext_lazy as _
 
-from asso.accountant.models import Transaction
-from asso.core.models.commons import Common, Created, Editable, Trashable
-
-models.CharField.register_lookup(Length)
+from ..commons.models import ContentModel, TitleModel
 
 
-class ApprovalState(Common, Trashable):
-    """Represents the state of approval of an Event."""
+class EventCategory(TitleModel):
+    """Represent the kind of event"""
 
-    show = models.BooleanField(
-        default=False,
-        verbose_name=_("Show At This State"),
-        help_text=_("Indicates if at this state the Event has to be shown"),
-    )
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Event Category")
+        verbose_name_plural = _("Event Categories")
 
 
-class Event(Created, Editable, Trashable):
-    """Represents a course, a talk or a conference.
+# class EventApprovalState(TitleModel):
+#     """Represents the state of approval of an Event."""
+#
+#     show = models.BooleanField(
+#         default=False,
+#         verbose_name=_("Show"),
+#         help_text=_("Indicates if at this state the Event has to be shown"),
+#     )
 
-    It can have some attendant teachers and can be carried out in several sessions.
-    """
 
-    slug = models.SlugField(unique=True, max_length=100)
+class Event(ContentModel):
+    """Represents a course, a talk or a conference."""
 
-    title = models.CharField(
-        max_length=200,
-        default="",
-        verbose_name=_("Title"),
-        help_text=_("Title of the event"),
-    )
     subtitle = models.CharField(
-        max_length=500,
+        max_length=250,
         default="",
         blank=True,
         verbose_name=_("Subtitle"),
-        help_text=_("Subtitle of the event"),
     )
 
-    approval_state = models.ForeignKey(
-        "ApprovalState",
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name="menu_entries",
-        verbose_name=_("Approval State"),
-        help_text=_("It represents the state of approval of the event"),
+    category = models.ForeignKey(
+        EventCategory,
+        on_delete=models.PROTECT,
+        related_name="events",
+        verbose_name=_("Category"),
+        help_text=_("The event category (course, talk, ...)"),
     )
 
     need_membership = models.BooleanField(
@@ -62,148 +54,158 @@ class Event(Created, Editable, Trashable):
     )
 
     price = models.DecimalField(
+        null=True,
+        blank=True,
         max_digits=14,
         decimal_places=4,
         default=0.0,
         verbose_name=_("Price"),
-        help_text=_("The price to pay in order to attend the Event"),
-    )
-
-    trainers = models.ManyToManyField(
-        "Trainer",
-        related_name="trainer_events",
-        verbose_name=_("Trainers"),
-        help_text=_("The Trainers that present the Event"),
-    )
-
-    # TODO: add automation to set this value when saving session to min date if null
-    start_date = models.DateField(
-        default=None,
-        null=True,
-        blank=True,
-        help_text=_("Set this value if you dont want to set sessions"),
-    )
-
-    # TODO: add automation to set this value when saving session to max date if null
-    end_date = models.DateField(
-        default=None,
-        null=True,
-        blank=True,
-        help_text=_("Set this value if you dont want to set sessions"),
+        help_text=_("The price to pay for this event (leave blank for free events)"),
     )
 
     def __str__(self):
         return f"{self.title}"
 
+    # trainers = models.ManyToManyField(
+    #     "Trainer",
+    #     related_name="trainer_events",
+    #     verbose_name=_("Trainers"),
+    #     help_text=_("The Trainers that present the Event"),
+    # )
 
-class Session(Created, Editable):
-    """If an Event it is divided in more session, it could be managed here."""
+    # approval_state = models.ForeignKey(
+    #     EventApprovalState,
+    #     on_delete=models.PROTECT,
+    #     related_name="events",
+    #     verbose_name=_("Approval State"),
+    #     help_text=_("Actual approval state for this event"),
+    # )
 
-    event = models.ForeignKey(
-        "Event",
-        on_delete=models.CASCADE,
-        related_name="event_sessions",
-        verbose_name=_("Event"),
-        help_text=_("The Event of witch the Session is referred to"),
-    )
+    # # TODO: add automation to set this value when saving session to min date if null
+    # start_date = models.DateField(
+    #     default=None,
+    #     null=True,
+    #     blank=True,
+    #     help_text=_("Set this value if you dont want to set sessions"),
+    # )
 
-    start = models.DateTimeField(
-        verbose_name=_("Start date and time"),
-        help_text=_(
-            "The start time and date of the session, determine hide state of the event"
-        ),
-    )
-
-    end = models.DateTimeField(
-        verbose_name=_("End date and time"),
-        help_text=_(
-            "The end time and date of the session, determine hide state of the event"
-        ),
-    )
-
-
-class Enrollment(Created, Editable):
-    """Indicate that User want to attend the Event."""
-
-    event = models.ForeignKey(
-        "Event",
-        on_delete=models.CASCADE,
-        related_name="event_enrollments",
-        verbose_name=_("Event"),
-        help_text=_("The Event of witch the Enrollment is referred to"),
-    )
-
-    attendant = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="event_attendants",
-        verbose_name=_("Attendant"),
-        help_text=_("The Attendant of witch the Enrollment is referred to"),
-    )
-
-    transaction = models.OneToOneField(
-        Transaction,
-        null=True,
-        on_delete=models.CASCADE,
-        related_name="transaction_enrollment",
-        verbose_name=_("Event"),
-        help_text=_("The Event of witch the Enrollment is referred to"),
-    )
+    # # TODO: add automation to set this value when saving session to max date if null
+    # end_date = models.DateField(
+    #     default=None,
+    #     null=True,
+    #     blank=True,
+    #     help_text=_("Set this value if you dont want to set sessions"),
+    # )
 
 
-class Presence(Created, Editable):
-    """Indicate the effective presence of an attendant in a particular session."""
+# class Session(Created, Editable):
+#     """If an Event it is divided in more session, it could be managed here."""
+#
+#     event = models.ForeignKey(
+#         "Event",
+#         on_delete=models.CASCADE,
+#         related_name="event_sessions",
+#         verbose_name=_("Event"),
+#         help_text=_("The Event of witch the Session is referred to"),
+#     )
+#
+#     start = models.DateTimeField(
+#         verbose_name=_("Start date and time"),
+#         help_text=_(
+#             "The start time and date of the session, determine hide state of the event"
+#         ),
+#     )
+#
+#     end = models.DateTimeField(
+#         verbose_name=_("End date and time"),
+#         help_text=_(
+#             "The end time and date of the session, determine hide state of the event"
+#         ),
+#     )
 
-    session = models.ForeignKey(
-        "Session",
-        on_delete=models.CASCADE,
-        related_name="session_presences",
-        verbose_name=_("Session"),
-        help_text=_("The Session for which the Presence is registered"),
-    )
 
-    enrollment = models.ForeignKey(
-        "Enrollment",
-        on_delete=models.CASCADE,
-        related_name="enrollment_presences",
-        verbose_name=_("Enrollment"),
-        help_text=_("The Enrollment of which the Presence is registered"),
-    )
+# class Enrollment(Created, Editable):
+#     """Indicate that User want to attend the Event."""
+#
+#     event = models.ForeignKey(
+#         "Event",
+#         on_delete=models.CASCADE,
+#         related_name="event_enrollments",
+#         verbose_name=_("Event"),
+#         help_text=_("The Event of witch the Enrollment is referred to"),
+#     )
+#
+#     attendant = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         related_name="event_attendants",
+#         verbose_name=_("Attendant"),
+#         help_text=_("The Attendant of witch the Enrollment is referred to"),
+#     )
+#
+#     transaction = models.OneToOneField(
+#         Transaction,
+#         null=True,
+#         on_delete=models.CASCADE,
+#         related_name="transaction_enrollment",
+#         verbose_name=_("Event"),
+#         help_text=_("The Event of witch the Enrollment is referred to"),
+#     )
 
 
-class Trainer(Created, Editable, Trashable):
-    """Represents someone that can present an Event"""
+# class Presence(Created, Editable):
+#     """Indicate the effective presence of an attendant in a particular session."""
+#
+#     session = models.ForeignKey(
+#         "Session",
+#         on_delete=models.CASCADE,
+#         related_name="session_presences",
+#         verbose_name=_("Session"),
+#         help_text=_("The Session for which the Presence is registered"),
+#     )
+#
+#     enrollment = models.ForeignKey(
+#         "Enrollment",
+#         on_delete=models.CASCADE,
+#         related_name="enrollment_presences",
+#         verbose_name=_("Enrollment"),
+#         help_text=_("The Enrollment of which the Presence is registered"),
+#     )
 
-    display_name = models.CharField(
-        _("Displayed name"), max_length=200, blank=True, default=""
-    )
 
-    biography = models.TextField(
-        blank=True,
-        verbose_name=_("Biography"),
-        help_text=_("Trainer Biography"),
-    )
-
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        verbose_name=_("User"),
-        help_text=_("The User the Trainer use for Login"),
-    )
-
-    @property
-    def template_name(self):
-        return self.user.full_name if not (dn := self.display_name) else dn
-
-    class Meta:
-        constraints = [
-            models.CheckConstraint(
-                check=Q(user__isnull=False) | Q(display_name__length__gt=0),
-                name="not_both_null",
-                violation_error_message=_(
-                    "displayed name and user cannot be either unset"
-                ),
-            )
-        ]
+# class Trainer(Created, Editable, Trashable):
+#     """Represents someone that can present an Event"""
+#
+#     display_name = models.CharField(
+#         _("Displayed name"), max_length=200, blank=True, default=""
+#     )
+#
+#     biography = models.TextField(
+#         blank=True,
+#         verbose_name=_("Biography"),
+#         help_text=_("Trainer Biography"),
+#     )
+#
+#     user = models.OneToOneField(
+#         settings.AUTH_USER_MODEL,
+#         null=True,
+#         blank=True,
+#         on_delete=models.SET_NULL,
+#         verbose_name=_("User"),
+#         help_text=_("The User the Trainer use for Login"),
+#     )
+#
+#     @property
+#     def template_name(self):
+#         return self.user.full_name if not (dn := self.display_name) else dn
+#
+#     class Meta:
+#         constraints = [
+#             models.CheckConstraint(
+#                 check=Q(user__isnull=False) | Q(display_name__length__gt=0),
+#                 name="not_both_null",
+#                 violation_error_message=_(
+#                     "displayed name and user cannot be either unset"
+#                 ),
+#             )
+#         ]
