eec3c283bc8cb6db27e0204c9d5ee6c29c15a9e9
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-10-05 15:14:36 +0200

    Require authentication to member pages

--- asso/member/views.py
+++ asso/member/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
 from . import models
 
 
-class MemberDetailView(generic.DetailView):
+class MemberDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "member/member.html"
     context_object_name = "member"
     model = models.Member
 
 
-class MemberCreateView(generic.CreateView):
+class MemberCreateView(LoginRequiredMixin, generic.CreateView):
     model = models.Member
     fields = ["user", "cf", "sex", "phone"]
