a5dca6356131f5e64a8ab49087d5bd212e3bf383
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-10-06 05:44:57 +0200

    Add support for multiple emails (closes #72)

--- asso/accounts/admin.py
+++ asso/accounts/admin.py
@@ -1,48 +1,52 @@
-import django.contrib.auth.admin
 from django.contrib import admin
 
-from .forms import UserCreateForm, UserUpdateForm
-from .models import User
+from .models import User, UserMail
+
+admin.site.register(User)
+admin.site.register(UserMail)
 
 
-@admin.register(User)
-class UserAdmin(django.contrib.auth.admin.UserAdmin):
-    """A Custom UserAdmin using email instead of username
+# class UserMailAdmin()
 
-    see: https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#a-full-example
-    see: https://testdriven.io/blog/django-custom-user-model/
-    """
 
-    list_display = [
-        "email",
-        "username",
-        "first_name",
-        "last_name",
-        "is_staff",
-        "is_superuser",
-        "last_login",
-    ]
-
-    list_filter = ["is_staff", "is_superuser"]
-    search_fields = ["email", "first_name", "last_name", "username"]
-    ordering = ["email"]
-
-    form = UserUpdateForm
-    add_form = UserCreateForm
-
-    add_fieldsets = [
-        (
-            None,
-            {
-                "classes": ["wide"],
-                "fields": [
-                    "email",
-                    "username",
-                    "first_name",
-                    "last_name",
-                    "password1",
-                    "password2",
-                ],
-            },
-        ),
-    ]
+# @admin.register(User)
+# class UserAdmin(django.contrib.auth.admin.UserAdmin):
+#     """A Custom UserAdmin using email instead of username
+#
+#     see: https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#a-full-example
+#     see: https://testdriven.io/blog/django-custom-user-model/
+#     """
+#
+#     list_display = [
+#         "email",
+#         "username",
+#         "first_name",
+#         "last_name",
+#         "is_staff",
+#         "is_superuser",
+#         "last_login",
+#     ]
+#
+#     list_filter = ["is_staff", "is_superuser"]
+#     search_fields = ["email", "first_name", "last_name", "username"]
+#     ordering = ["email"]
+#
+#     form = UserUpdateForm
+#     add_form = UserCreateForm
+#
+#     add_fieldsets = [
+#         (
+#             None,
+#             {
+#                 "classes": ["wide"],
+#                 "fields": [
+#                     "email",
+#                     "username",
+#                     "first_name",
+#                     "last_name",
+#                     "password1",
+#                     "password2",
+#                 ],
+#             },
+#         ),
+#     ]
--- asso/accounts/models.py
+++ asso/accounts/models.py
@@ -8,66 +8,54 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, password, **fields):
         """Create and save a user with the given email and password."""
-        if not email:
-            raise ValueError(_("The Email must be set"))
+        if not (email := fields.pop("email", None)):
+            raise ValueError("Users must have an email address")
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+
+        user = self.model(username=username, email=email, **fields)
         user.set_password(password)
         user.save()
+
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **fields):
         """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
+        fields.setdefault("is_staff", True)
+        fields.setdefault("is_superuser", True)
+        fields.setdefault("is_active", True)
 
-        if extra_fields.get("is_staff") is not True:
+        if fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
+        if fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **fields)
+
+
+class UserMail(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
+    email = models.EmailField(unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return f"<{self.user.username}> {self.email}"
 
 
 class User(AbstractUser):
-    email = models.EmailField(_("email address"), unique=True)
-
-    first_name = models.CharField(
-        _("First Name"),
-        max_length=150,
-        blank=False,
-    )
-    last_name = models.CharField(
-        _("Last Name"),
-        max_length=150,
-        blank=False,
-    )
-
-    username_validator = UnicodeUsernameValidator()
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        null=True,
-        blank=True,
-        help_text=_(
-            "Optional, 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[username_validator],
-    )
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    email = models.EmailField(unique=True, null=False, blank=False)
 
     objects = UserManager()
 
-    @property
-    def full_name(self) -> str:
-        return self.get_full_name()
+    def save(self, *args, **kwargs):
+        query = UserMail.objects.filter(email=self.email)
+        if self.pk:
+            query = query.exclude(user__id=self.id)
 
-    def __str__(self):
-        if full_name := self.get_full_name():
-            return f"{full_name} ({self.email})"
-        return self.email
+        if query.exists():
+            raise ValueError("email already taken")
+
+        super().save(*args, **kwargs)
+
+        if not UserMail.objects.filter(email=self.email).exists():
+            UserMail.objects.create(user=self, email=self.email)
