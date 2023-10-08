3dd7331a2942c14a86226f9150400b71d323b0e3
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-10-08 22:35:34 +0200

    Add Base Model for Address models

--- asso/address/models.py
+++ asso/address/models.py
@@ -10,10 +10,10 @@ def validate_uppercase(value: str):
 
 class Country(TitleModel, OrderedModel):
     code = models.CharField(
-        _("Country Code (ISO-3166)"),
+        f"{_('Country Code')} (ISO-3166)",
         unique=True,
         max_length=2,
-        help_text=_("A two letter code to identify country (use ISO-3166)"),
+        help_text=_("A two letter code to identify country (based on ISO-3166)"),
         validators=[validate_uppercase],
     )
 
@@ -29,8 +29,8 @@ class Region(TitleModel, OrderedModel):
     code = models.CharField(
         _("Region Code"),
         max_length=2,
-        help_text=_(
-            "A two letter code to identify a state, region, province inside a country"
+        help_text=(
+            _("A two letter code to identify a state, region, province in a country")
         ),
         validators=[validate_uppercase],
     )
@@ -38,7 +38,7 @@ class Region(TitleModel, OrderedModel):
         Country,
         on_delete=models.CASCADE,
         verbose_name=_("Country"),
-        help_text=_("This The country this Region belongs to"),
+        help_text=_("The country this Region belongs to"),
     )
 
     class Meta:
@@ -51,3 +51,39 @@ class Region(TitleModel, OrderedModel):
             )
         ]
 
+
+class AddressBaseModel(models.Model):
+    line = models.CharField(
+        _("Address line"),
+        max_length=200,
+        help_text=_("Example: Via Barchetta 77"),
+    )
+    additional_line = models.CharField(
+        _("Additional Address info"),
+        blank=True,
+        max_length=200,
+        help_text=_("Additional info"),
+    )
+
+    city = models.CharField(
+        _("City"),
+        max_length=100,
+        help_text=_("Example: Modena"),
+    )
+
+    zip_code = models.CharField(_("Postal Code"), max_length=5)
+
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.PROTECT,
+        verbose_name=_("Region/Province/State"),
+        help_text=_("Region/province/state code (EE for Foreign Country"),
+    )
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.PROTECT,
+        verbose_name=_("Country"),
+    )
+
+    class Meta:
+        abstract = True
