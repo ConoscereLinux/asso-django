4fd643ef518fabc2409d13e73b29f1c5e0951896
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-11-02 22:16:02 +0100

    Add events page

--- asso/academy/urls.py
+++ asso/academy/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.EventList.as_view(), name="events"),
     path("<slug:slug>/", views.EventDetailView.as_view(), name="event"),
 ]
--- asso/academy/views.py
+++ asso/academy/views.py
@@ -3,6 +3,12 @@ from django.views import generic
 from . import models
 
 
+class EventList(generic.ListView):
+    model = models.Event
+    template_name = "academy/events.html"
+    context_object_name = "courses"
+
+
 class EventDetailView(generic.DetailView):
     model = models.Event
     template_name = "academy/event.html"
