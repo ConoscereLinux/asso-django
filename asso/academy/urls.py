437aafb9757ff36ec1dedfe6b019b9e3db0ddccc
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-11-03 02:14:09 +0100

    Add redirect from academy/ to academy/list

--- asso/academy/urls.py
+++ asso/academy/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="events")),
     path("list/", views.EventList.as_view(), name="events"),
     path("detail/<slug:slug>/", views.EventDetail.as_view(), name="event"),
     path("create/", views.EventCreate.as_view(), name="event-create"),
